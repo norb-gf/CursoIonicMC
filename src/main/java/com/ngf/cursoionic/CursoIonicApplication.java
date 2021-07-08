777c3848abb42ee235107c7eda3e9eb69ec3a626
norb_gf <norb_gf@yahoo.com.br>
2021-07-08 18:46:15 -0300

    Projeto criado

---
+++ src/main/java/com/ngf/cursoionic/CursoIonicApplication.java
@@ -0,0 +1,13 @@
+package com.ngf.cursoionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngf/cursoionic/CursoIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngf.cursoionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
