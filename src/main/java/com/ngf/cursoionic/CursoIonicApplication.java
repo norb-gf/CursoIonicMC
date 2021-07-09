5df37ac7d2dcf78dd4dad9d4289465f787422a5f
norb_gf <norb_gf@yahoo.com.br>
2021-07-09 09:29:37 -0300

    Criando instanciação Categorias

--- src/main/java/com/ngf/cursoionic/CursoIonicApplication.java
+++ src/main/java/com/ngf/cursoionic/CursoIonicApplication.java
@@ -1,13 +1,30 @@
 package com.ngf.cursoionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoIonicApplication {
+import com.ngf.cursoionic.domain.Categoria;
+import com.ngf.cursoionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoIonicApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoIonicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
