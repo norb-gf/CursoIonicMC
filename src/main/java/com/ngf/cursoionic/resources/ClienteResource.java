97346e0218eabd580a37c02832331cab2586de26
norb_gf <norb_gf@yahoo.com.br>
2021-07-24 16:39:01 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/ngf/cursoionic/resources/ClienteResource.java
+++ src/main/java/com/ngf/cursoionic/resources/ClienteResource.java
@@ -19,48 +19,47 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ngf.cursoionic.domain.Cliente;
 import com.ngf.cursoionic.dto.ClienteDTO;
+import com.ngf.cursoionic.dto.ClienteNewDTO;
 import com.ngf.cursoionic.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-	
-		Cliente obj = service.find(id);		
+
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
 		List<ClienteDTO> listDTO = list.stream().map(cat -> new ClienteDTO(cat)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	@RequestMapping(value="/page",method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-				@RequestParam(value="page", defaultValue="0") Integer page,
-				@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-				@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
-				@RequestParam(value="direction", defaultValue="ASC") String direction ) {
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDTO = list.map(cat -> new ClienteDTO(cat));
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
-	/*
-	 * @RequestMapping(method = RequestMethod.POST) public ResponseEntity<Void>
-	 * insert(@Valid @RequestBody ClienteDTO objDto) { Cliente obj =
-	 * service.fromDTO(objDto); obj = service.insert(obj); URI uri =
-	 * ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand
-	 * (obj.getId()).toUri(); return ResponseEntity.created(uri).build(); }
-	 */
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/com/ngf/cursoionic/services/ClienteService.java
+++ src/main/java/com/ngf/cursoionic/services/ClienteService.java
@@ -9,10 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ngf.cursoionic.domain.Cidade;
 import com.ngf.cursoionic.domain.Cliente;
+import com.ngf.cursoionic.domain.Endereco;
+import com.ngf.cursoionic.domain.enums.TipoCliente;
 import com.ngf.cursoionic.dto.ClienteDTO;
+import com.ngf.cursoionic.dto.ClienteNewDTO;
+import com.ngf.cursoionic.repositories.CidadeRepository;
 import com.ngf.cursoionic.repositories.ClienteRepository;
+import com.ngf.cursoionic.repositories.EnderecoRepository;
 import com.ngf.cursoionic.services.exceptions.DataIntegrityException;
 import com.ngf.cursoionic.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +29,12 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -37,9 +50,13 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 	}
 
-	/*
-	 * public Cliente insert(Cliente obj){ obj.setId(null); return repo.save(obj); }
-	 */
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -60,7 +77,24 @@ public class ClienteService {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
