80028bedc0c447e27addd6bbc9f15eebb6073113
norb_gf <norb_gf@yahoo.com.br>
2021-07-26 16:05:18 -0300

    Criando profile de Desenvolvimento

---
+++ src/main/java/com/ngf/cursoionic/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.ngf.cursoionic.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ngf.cursoionic.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+
+}
