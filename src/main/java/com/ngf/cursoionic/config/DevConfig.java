a371e2453b22009e6fa26ce4b66e2dc0e8aeee36
norb_gf <norb_gf@yahoo.com.br>
2021-07-27 18:14:39 -0300

    Implementando SmtpEmailService com servidor de Google

--- src/main/java/com/ngf/cursoionic/config/DevConfig.java
+++ src/main/java/com/ngf/cursoionic/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ngf.cursoionic.services.DBService;
+import com.ngf.cursoionic.services.EmailService;
+import com.ngf.cursoionic.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,4 +32,9 @@ public class DevConfig {
 		return true;
 	}
 
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
 }
--- src/main/java/com/ngf/cursoionic/services/DBService.java
+++ src/main/java/com/ngf/cursoionic/services/DBService.java
@@ -115,7 +115,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSSOA_FíSICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "norb_gf@yahoo.com.br", "36378912377", TipoCliente.PESSSOA_FíSICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("12345654", "987654321"));
 
