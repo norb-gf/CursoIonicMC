01aee1b24afa515f95364c841f84a30e22f693b1
norb_gf <norb_gf@yahoo.com.br>
2021-07-27 22:42:50 -0300

    Configuracao Inicial do Spring Security

--- src/main/java/com/ngf/cursoionic/services/AbstractEmailService.java
+++ src/main/java/com/ngf/cursoionic/services/AbstractEmailService.java
@@ -29,7 +29,7 @@ public abstract class AbstractEmailService implements EmailService {
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
-		sendEmail(sm);
+//		sendEmail(sm);
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
@@ -51,25 +51,24 @@ public abstract class AbstractEmailService implements EmailService {
 	@Override
 	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
 		try {
-			
+
 			MimeMessage mm = prepareMimeMessageFromPedido(obj);
-			sendHtmlEmail(mm);
-		}
-		catch (MessagingException e) {
+//			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
 			sendOrderConfirmationEmail(obj);
 		}
-	
+
 	}
 
 	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
- 		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setTo(obj.getCliente().getEmail());
 		mmh.setFrom(sender);
 		mmh.setSubject("Pedido Confirmado!! Código: " + obj.getId());
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
 	}
- 
+
 }
