d9ac253ab954ffaa7b714bc3668ba56861ab38ca
norb_gf <norb_gf@yahoo.com.br>
2021-08-06 22:04:12 -0300

    Enviando imagem via endpoint

--- src/main/java/com/ngf/cursoionic/CursoIonicApplication.java
+++ src/main/java/com/ngf/cursoionic/CursoIonicApplication.java
@@ -10,19 +10,12 @@ import com.ngf.cursoionic.services.S3Service;
 @SpringBootApplication
 public class CursoIonicApplication implements CommandLineRunner {
 
-
-	@Autowired
-	private S3Service s3Service;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursoIonicApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\finan\\Documents\\_Norb\\Cursos\\Ionic\\ProjIonicSTS\\Imagens\\Tapete1.jpg");
-		
-
 
 	}
 }
